496e93abf5e425a91fb54e4d0f6f75f4cce43dbd
Patience-Kori1 <koribiram@yahoo.fr>
2025-09-14 20:24:26 +0200

    feat(maman): parent component with event handling - Created Maman component with state (messageMaman, messageToto) - Added ordreMaman method to update the state - Integrated Maman component in App.js with message rendering

---
+++ src/components/Maman.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+
+class Maman extends Component {
+
+    state = {
+        messageMaman: null,
+        messageToto : null
+    }
+
+    ordreMaman = () => {
+        this.setState({
+           messageMaman: "Va ranger ta chambre" 
+        })
+    }
+
+    render(){
+
+        return (
+            <div>
+                <h1>Maman</h1>
+                <button onClick={this.ordreMaman}>Ordre de la mère</button>
+                <p>{this.state.messageMaman}</p>
+            </div>
+        )
+    }
+}
+
+export default Maman
