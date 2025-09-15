a0fb13ac4fdfa6e62e07103402bf87d2b0dd295e
Patience-Kori1 <koribiram@yahoo.fr>
2025-09-15 10:55:27 +0200

    feat(parent-child): Parameterized and flexible second approach for Maman-Toto communication - Created secondMethode folder and added Maman.js and Toto.js - Added ordreMaman method accepting a dynamic message as parameter - Added reponseToto method allowing Toto to send a custom reply - Implemented disabled state in Maman to control activation of the Response button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Maman from './components/Maman'
+import Maman from './secondMethode/Maman'
+// import Maman from './components/Maman'
 
 function App() {
   
