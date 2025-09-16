1d5f7c6df385e4ad4beeef43d7cd4bfd0a5291ae
Patience-Kori1 <koribiram@yahoo.fr>
2025-09-16 20:33:56 +0200

    feat(form): Added color selection and comment handling - Added color object and comment variable in the state - Created a select dropdown for color choice and textarea for comment input - Implemented handleColor and handleComments to update the state - Dynamically displayed user comment and applied color styling to name and comment

--- src/secondMethode/Form.jsx
+++ src/secondMethode/Form.jsx
@@ -5,12 +5,26 @@ export class Form extends Component {
 
   state = {
     username:'',
-    
+    color: '',
+    colors : ["", "red", "Indigo", "DarkGreen", "DarkBlue", "chocolate"], // on met le vide pour que la première valeur soit vide
+    comment : ''
   }
 
   handlePseudo = e => {
     this.setState({
       username: e.target.value,
+    })
+  }
+
+  handleColor = e => {
+    this.setState({
+      color: e.target.value
+    })
+  }
+
+  handleComments = e => {
+    this.setState({
+      comment: e.target.value
     })
   }
 
@@ -25,11 +39,42 @@ export class Form extends Component {
           <div className="commentForm">
             <p className= " userName "style = {{backgroundColor :this.state.color}}>Affichage pseudo :  <span>{this.state.username}</span></p>
 
+            <p className= " userComment " style = {{ color :this.state.color}}>Commentaire  :  <span>{this.state.comment}</span></p>
+            <label htmlFor="Pseudo">Saisie pseudo : </label>
+
             <input 
               type="text" 
               value={this.state.username}
               onChange={this.handlePseudo}
             /> 
+          </div>
+
+          <div className="commentForm">
+              <label htmlFor="color" >Couleur : </label>
+
+              <select 
+                name="color" 
+                value= {this.state.color} 
+                onChange={this.handleColor}
+              >
+
+                {
+                  this.state.colors.map((color, index) =>{
+                    return <option key={index} value= {color}> {color}</option>
+                  })
+                }
+
+                {/* Si on implémente les option du select en dure et non avec map()
+                <option value="" className="vert"> vert</option>
+                <option value="" className="rouge"> rouge </option>
+                <option value="" className="orange"> orange </option> */}
+
+              </select>
+          </div>
+
+          <div className="commentForm">
+            <label htmlFor="">Commentaire</label>
+            <textarea value= {this.state.comment} onChange={this.handleComments}></textarea>
           </div>
 
           <Car color="red" height="100"/>
