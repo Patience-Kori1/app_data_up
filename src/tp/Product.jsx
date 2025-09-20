77624315f42f21cf1c1ccc55ba82bc7d8f43e0e6
Patience-Kori1 <koribiram@yahoo.fr>
2025-09-20 23:56:39 +0200

    feat(display): Conditional rendering of Product and ReviewForm components - Added isVisible set to true in StyleBoutik to control rendering - Implemented isNotVisible and isProductVisible methods for toggling display - Passed these methods as props to Product and ReviewForm - Added button triggering isNotVisible to hide Product

--- src/tp/Product.jsx
+++ src/tp/Product.jsx
@@ -6,7 +6,7 @@ import StanSmith from  './image_product_react/Stan_Smith_Vert.jpg'
 import SweatCapuche from  './image_product_react/Sweat-a-Capuche.webp'
 import TommyPull from  './image_product_react/Tommy-Pull-Rouge.webp'
 import './tp.css'
-
+import ReviewForm from './ReviewForm'
 export class Product extends Component {
 
   state = {
@@ -24,31 +24,54 @@ export class Product extends Component {
   
     
   render() {
-    
+
+    // console.log(this.props.unVisible)
+
+    // console.log(this.props.visible)
+
     return (
       <div className="Product">
         <h2>Notre collection de produit</h2>
+
         <div className='imgDiv'>
           {
            this.state.products.map((value, index)=>{
               return (
-                <div>
+                <div key={index} >
                       <img src={this.state.products[index].src} alt="" className='img'/>
+
                       <h4>{this.state.products[index].name}</h4>
-                      <button class="btn btn-primary" type="button">
-                        Donner votre avis sur : <span> </span> 
+
+                      <button 
+                        className="btn btn-primary" 
+                        type="button"
+                        onClick={this.props.unVisible}
+
+                      >
+
+                          Donner votre avis sur : <span> </span> 
                         <img 
                           src={this.state.products[index].src} 
-                          className='imgSmall'
-                          
+                          className='imgSmall'  
                         />
                       </button>
+                      {/* {console.log(this.props.unVisible)} */}
 
+                    {/* <div className="hidden">
+                      <ReviewForm
+                      
+                      srcArticle= {this.state.products[index].src} 
+                      />
+                    </div> */}
+                      
+                      
                 </div>
-            )
-           })
+              )
+            })
           }
         </div>
+
+        
         
       </div>
     )
--- src/tp/StyleBoutik.jsx
+++ src/tp/StyleBoutik.jsx
@@ -1,18 +1,63 @@
 import React, { Component } from 'react'
 import Product from './product'
+import ReviewForm from './ReviewForm'
 
 export class StyleBoutik extends Component {
 
-  state = {
-    
-  }
-    
+state = {
+  isVisible: true
+}
+
+   isNotVisible = () => {
+    this.setState({
+      isVisible:false
+    })
+   } 
+
+   isProductVisible = () => {
+    this.setState({
+      isVisible:true
+    })
+   } 
+  
   render() {
+    // {console.log(this.state.isVisible)};
+
+    const productFalse = this.state.isVisible === true ? (
+
+                  <div>
+                    <Product 
+                      visible={this.state.isVisible} 
+                      unVisible={this.isNotVisible}
+                  
+                    />
+
+                    
+                  </div>
+
+                    
+                      
+                    )  : null;
+
+
+
     return (
       <div>
 
-        <Product/>
+        {productFalse}
+
+        {/* <Product visible={this.state.isVisible} /> */}
+        {/* {console.log(this.isProductVisible)} */}
+
+        <ReviewForm 
+          
+          visible={this.state.isVisible}
+          unVisible={this.isProductVisible}
         
+        />
+
+        <button onClick={this.isNotVisible}>isNotVisible</button> 
+
       </div>
     )
   }
